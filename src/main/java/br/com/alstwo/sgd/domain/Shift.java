4a4eeb787e8f5385faa0c41a8b3834e9cef8c460
Alisson_PersonalNot <alissoncavalcanticma@gmail.com>
2025-05-11 02:58:47 -0300

    Adjust findAllFilters

--- src/main/java/br/com/alstwo/sgd/domain/Shift.java
+++ src/main/java/br/com/alstwo/sgd/domain/Shift.java
@@ -1,13 +1,5 @@
 package br.com.alstwo.sgd.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.Date;
-
 public class Shift {
 
 
