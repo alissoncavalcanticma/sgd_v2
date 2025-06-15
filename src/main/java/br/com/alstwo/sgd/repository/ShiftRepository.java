3525ee4fd351d2da2b7566be27c3e09a5e6c2607
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2025-06-14 23:30:16 -0300

    Implementing Shift and ShiftRecord class of model and repository | Adjust DTO of User

--- src/main/java/br/com/alstwo/sgd/repository/ShiftRepository.java
+++ src/main/java/br/com/alstwo/sgd/repository/ShiftRepository.java
@@ -2,8 +2,7 @@ package br.com.alstwo.sgd.repository;
 
 import br.com.alstwo.sgd.domain.Shift;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-public interface ShiftRespository extends JpaRepository<Shift, Long>{
+public interface ShiftRepository extends JpaRepository<Shift, Long>{
 
 }
