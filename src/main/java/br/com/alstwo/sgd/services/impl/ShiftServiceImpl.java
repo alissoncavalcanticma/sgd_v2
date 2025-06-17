27a59e9ebe36e0c7820290e73fc5c31f2b41f442
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2025-06-16 23:08:22 -0300

    Implementing Shift service part 2

--- src/main/java/br/com/alstwo/sgd/services/impl/ShiftServiceImpl.java
+++ src/main/java/br/com/alstwo/sgd/services/impl/ShiftServiceImpl.java
@@ -5,9 +5,11 @@ import br.com.alstwo.sgd.repository.ShiftRepository;
 import br.com.alstwo.sgd.services.ShiftService;
 import br.com.alstwo.sgd.services.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 @RequiredArgsConstructor
 public class ShiftServiceImpl implements ShiftService {
 
@@ -20,21 +22,23 @@ public class ShiftServiceImpl implements ShiftService {
 
     @Override
     public List<Shift> findAll() {
-        return List.of();
+        return shiftRepository.findAll();
     }
 
     @Override
     public Shift create(Shift shift) {
-        return null;
+        return shiftRepository.save(shift);
     }
 
     @Override
     public Shift update(Shift shift) {
-        return null;
+        shiftRepository.findById(shift.getId()).orElseThrow(() -> new ObjectNotFoundException("Turno não encontrado."));
+        return shiftRepository.save(shift);
     }
 
     @Override
-    public void delete(Long Id) {
-
+    public void delete(Long id) {
+        shiftRepository.findById(id).orElseThrow(()-> new ObjectNotFoundException("Turno não encontrado."));
+        shiftRepository.deleteById(id);
     }
 }
