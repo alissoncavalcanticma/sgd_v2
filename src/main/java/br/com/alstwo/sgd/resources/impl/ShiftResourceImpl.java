48662aa827311d6d8ae5f72e58333d92b5d2c2ab
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2025-06-19 14:20:30 -0300

    Implementing ShiftResource.java Interface and create ShiftResourceImpl.java

---
+++ src/main/java/br/com/alstwo/sgd/resources/impl/ShiftResourceImpl.java
@@ -0,0 +1,29 @@
+package br.com.alstwo.sgd.resources.impl;
+
+import br.com.alstwo.sgd.domain.dto.ShiftDTO;
+import br.com.alstwo.sgd.resources.ShiftResource;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public class ShiftResourceImpl implements ShiftResource {
+    @Override
+    public ResponseEntity<List<ShiftDTO>> findAll() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<ShiftDTO> create(ShiftDTO shiftDTO) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<ShiftDTO> update(Long id, ShiftDTO shiftDTO) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> delete(Long id) {
+        return null;
+    }
+}
