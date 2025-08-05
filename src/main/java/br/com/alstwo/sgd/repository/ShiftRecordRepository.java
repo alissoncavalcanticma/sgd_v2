3a26c92e823f6515be3a5ea1dca73bd5135beac4
Alisson_PersonalNot <alissoncavalcanticma@gmail.com>
2025-08-05 00:25:34 -0300

    Creating ShiftRecordServiceImpl class

--- src/main/java/br/com/alstwo/sgd/repository/ShiftRecordRepository.java
+++ src/main/java/br/com/alstwo/sgd/repository/ShiftRecordRepository.java
@@ -2,7 +2,31 @@ package br.com.alstwo.sgd.repository;
 
 import br.com.alstwo.sgd.domain.ShiftRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface ShiftRecordRepository extends JpaRepository<ShiftRecord, Long>{
 
+    @Query(nativeQuery = true, value = """
+            SELECT *
+            FROM turnos_registro tr
+            WHERE (:id IS NULL OR tr.id = :id)
+            AND (:userId IS NULL OR tr.usuario_id = :userId)
+            AND (:shiftId IS NULL OR tr.turno_id = :shiftId)
+            AND (
+                    (:startDate IS NULL OR tr.data >= :startDate)
+                    AND (:endDate IS NULL OR tr.data <= :endDate)
+                )
+            AND (:status IS NULL OR tr.status = :status)
+            """)
+    List<ShiftRecord> findByAllFilters(
+            Long id,
+            Long userId,
+            Long shiftId,
+            LocalDate startDate,
+            LocalDate endDate,
+            Integer status);
 }
--- src/main/java/br/com/alstwo/sgd/services/impl/ShiftRecordServiceImpl.java
+++ src/main/java/br/com/alstwo/sgd/services/impl/ShiftRecordServiceImpl.java
@@ -1,16 +1,24 @@
 package br.com.alstwo.sgd.services.impl;
 
 import br.com.alstwo.sgd.domain.ShiftRecord;
+import br.com.alstwo.sgd.repository.ShiftRecordRepository;
 import br.com.alstwo.sgd.services.ShiftRecordService;
+import br.com.alstwo.sgd.services.exceptions.ObjectNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class ShiftRecordServiceImpl implements ShiftRecordService {
+
+    private final ShiftRecordRepository shiftRecordRepository;
 
     @Override
     public ShiftRecord findById(Long id) {
-        return null;
+        return shiftRecordRepository.findById(id).orElseThrow(()-> new ObjectNotFoundException("Registro de turno não encontrado"));
     }
 
     @Override
