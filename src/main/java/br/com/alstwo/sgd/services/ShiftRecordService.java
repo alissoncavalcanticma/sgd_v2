4ebfa80d9761372fa280af53177395181e929907
Alisson_PersonalNot <alissoncavalcanticma@gmail.com>
2025-08-04 23:12:17 -0300

    Creating ShiftRecordService interface

---
+++ src/main/java/br/com/alstwo/sgd/services/ShiftRecordService.java
@@ -0,0 +1,15 @@
+package br.com.alstwo.sgd.services;
+
+import br.com.alstwo.sgd.domain.ShiftRecord;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface ShiftRecordService {
+
+    ShiftRecord findById(Long id);
+    List<ShiftRecord> findByAllFilters (Long id, Long userId, Long shiftId, LocalDateTime opening, LocalDateTime closing, Integer status);
+    ShiftRecord create(ShiftRecord shiftRecord);
+    ShiftRecord update(ShiftRecord shiftRecord);
+    void delete(Long id);
+}
