ded5cda67b204fd97fd00101ba3cd904ece7a1b7
Alisson_PersonalNot <alissoncavalcanticma@gmail.com>
2025-08-05 23:35:25 -0300

    Adjusting ShiftRecordRepository class

--- src/main/java/br/com/alstwo/sgd/repository/ShiftRecordRepository.java
+++ src/main/java/br/com/alstwo/sgd/repository/ShiftRecordRepository.java
@@ -2,8 +2,8 @@ package br.com.alstwo.sgd.repository;
 
 import br.com.alstwo.sgd.domain.ShiftRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -23,10 +23,10 @@ public interface ShiftRecordRepository extends JpaRepository<ShiftRecord, Long>{
             AND (:status IS NULL OR tr.status = :status)
             """)
     List<ShiftRecord> findByAllFilters(
-            Long id,
-            Long userId,
-            Long shiftId,
-            LocalDate startDate,
-            LocalDate endDate,
-            Integer status);
+            @Param("id") Long id,
+            @Param("userId") Long userId,
+            @Param("shiftId") Long shiftId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("status") Integer status);
 }
